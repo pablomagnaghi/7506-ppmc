5144ee9160fb2a4e6d8edd6d3a425104d50ae7ca
carlos.pantelides <carlos.pantelides@gmail.com>
2011-03-17 20:43:31 +0000

    some refactoring

--- pruebas_carlos_cppunit/hex_coder/v04/hexdecoder_test.hpp
+++ pruebas_carlos_cppunit/hex_coder/v04/hexdecoder_test.hpp
@@ -1,12 +1,15 @@
 #ifndef hexdecoder_h
 #define hexdecoder_h
 
+#include <string>
+
 #include <cppunit/extensions/HelperMacros.h>
 
 class HexDecoderTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE( HexDecoderTest );
   CPPUNIT_TEST( testConstructor );
   CPPUNIT_TEST( testDecode );
+  CPPUNIT_TEST( testGetAsString);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -16,6 +19,10 @@ public:
   void testDecode(); 
   void testGetCharBufferPtr();
   void testGetCharBufferCopy();
+  void testGetAsString();
+private:
+  std::string input;
+  static unsigned char expected[];
 };
 
 #endif  // hexdecoder_h
