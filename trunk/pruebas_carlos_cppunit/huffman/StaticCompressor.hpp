e3b0ff2f925c2b8e1c13db229928c7ac7ee5fbc9
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-03-25 20:54:07 +0000

    inicio huffman

---
+++ trunk/pruebas_carlos_cppunit/huffman/StaticCompressor.hpp
@@ -0,0 +1,14 @@
+#ifndef _huffman_static_compressor_hpp_
+#define _huffman_static_compressor_hpp_
+#include "Huffman.hpp"
+
+namespace huffman {
+
+
+  class StaticCompressor {
+    // use tree
+    // compress(tree,infile,outfile);
+  };
+}
+
+#endif // _huffman_static_compressor_hpp_
