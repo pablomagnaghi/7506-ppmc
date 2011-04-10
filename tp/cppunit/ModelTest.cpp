e8c995c7b3dd3100310368c1669de9d2886c3b9a
carlos.pantelides <carlos.pantelides@gmail.com>
2011-04-10 01:47:05 +0000

    fin agregado del resto de las clases centrales con test vacios

---
+++ tp/cppunit/ModelTest.cpp
@@ -0,0 +1,23 @@
+#include "ModelTest.h"
+#include "../PPMC.h"
+#include "../Model.h"
+
+using namespace ppmc;
+using namespace std;
+
+CPPUNIT_TEST_SUITE_REGISTRATION( ModelTest );
+
+
+void ModelTest::setUp(){
+
+}
+
+
+void ModelTest::tearDown(){
+}                               
+
+
+void ModelTest::testConstructor(){
+	
+}
+
