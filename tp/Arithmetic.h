9c758dcfb030ecbef580e4ea953cc7105eafb5e6
carlos.pantelides <carlos.pantelides@gmail.com>
2011-04-16 00:21:05 +0000

    mas clases e inicio de codigo de Model

---
+++ tp/Arithmetic.h
@@ -0,0 +1,19 @@
+#ifndef _ppmc_arithmetic_h_
+#define _ppmc_arithmetic_h_
+
+#include <vector>
+#include "PPMC.h"
+#include "Model.h"
+
+
+namespace ppmc {
+	class Arithmetic {
+		public:
+			Arithmetic();
+			virtual ~Arithmetic();
+		protected:
+			std::vector<Model> models;
+		friend class CompressorTest;
+	};
+}
+#endif //_ppmc_arithmetic_h_
--- tp/Model.cpp
+++ tp/Model.cpp
@@ -12,3 +12,13 @@ Model::~Model(){
 	// ver si el destructor de std::map llama a los destructores de lo que tiene, 
 	// si no hay que limpiar aca
 }
+
+ContextTable* Model::find(ContextSelector& cs, size_t depth){
+	std::map<std::string, ContextTable*>::iterator it;
+	it = contextTables.find(cs.get(depth));
+	if (it == contextTables.end()) {
+		return new ContextTable();
+		// dont forget to delete this somewhere
+	}
+	return it->second;
+}
