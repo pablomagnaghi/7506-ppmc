05744ba880d65615a51def9d1cb597d889aede0e
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-04-17 15:28:34 +0000

    simplificacion de ContextSelector, inicio de implementacion de lista de exclusion

--- trunk/tp/cppunit/ContextSelectorTest.cpp
+++ trunk/tp/cppunit/ContextSelectorTest.cpp
@@ -33,46 +33,22 @@ void ContextSelectorTest::testBadConstructor(){
 	//ContextSelector e(16);
 //	CPPUNIT_ASSERT_THROW( ContextSelector e(15), std::length_error* );
 }
+
 void ContextSelectorTest::testAdd(){
 	ContextSelector e(3);
-	string reference("a");
-	string expected_1("ab");
-	string expected_0("abc");
 	
-	e.set(reference);
-	CPPUNIT_ASSERT_EQUAL(reference,e.get(0));
-	e.add(string("b"));
-	CPPUNIT_ASSERT_EQUAL(expected_1, e.get(0));
+	e.add('a');
+	CPPUNIT_ASSERT_EQUAL(string("a"),e.get(1));
+	e.add('b');
+	CPPUNIT_ASSERT_EQUAL(string("b"),e.get(1));
+	CPPUNIT_ASSERT_EQUAL(string("ab"),e.get(2));
 	e.add('c');
-	CPPUNIT_ASSERT_EQUAL(expected_0, e.get(0));
+	CPPUNIT_ASSERT_EQUAL(string("c"),e.get(1));
+	CPPUNIT_ASSERT_EQUAL(string("bc"),e.get(2));
+	CPPUNIT_ASSERT_EQUAL(string("abc"),e.get(3));
+	e.add('d');
+	CPPUNIT_ASSERT_EQUAL(string("d"),e.get(1));
+	CPPUNIT_ASSERT_EQUAL(string("cd"),e.get(2));
+	CPPUNIT_ASSERT_EQUAL(string("bcd"),e.get(3));
 
-}
-
-void ContextSelectorTest::testSet(){
-	ContextSelector e(3);
-	string reference("abc");
-	e.set(reference);
-	CPPUNIT_ASSERT_EQUAL(reference,e.get(0));
-}
-
-void ContextSelectorTest::testGet(){
-	ContextSelector e(3);
-	string reference("abc");
-	string expected_0("abc");
-	string expected_1("bc");
-	string expected_2("c");
-	string expected_3("");
-	e.set(reference);
-	CPPUNIT_ASSERT_EQUAL(expected_0, e.get(0));
-	CPPUNIT_ASSERT_EQUAL(expected_1, e.get(1));
-	CPPUNIT_ASSERT_EQUAL(expected_2, e.get(2));
-	CPPUNIT_ASSERT_EQUAL(expected_3, e.get(3));
-}
-
-void ContextSelectorTest::testAddBadSize() {
-	
-}
-
-void ContextSelectorTest::testAddBadSize0() {
-	
 }
--- trunk/tp/cppunit/ContextTableTest.cpp
+++ trunk/tp/cppunit/ContextTableTest.cpp
@@ -83,6 +83,7 @@ void ContextTableTest::testCompress_a_a(){
 	Query q;
 	q.setTerm('a');
 	c.compress(q);
+	q.setTerm('a');
 	c.compress(q);
 	
 	Probability p = q.getProbability();
@@ -98,7 +99,9 @@ void ContextTableTest::testCompress_a_a_a(){
 	Query q;
 	q.setTerm('a');
 	c.compress(q);
+	q.setTerm('a');
 	c.compress(q);
+	q.setTerm('a');
 	c.compress(q);
 	
 	Probability p = q.getProbability();
@@ -126,4 +129,27 @@ void ContextTableTest::testCompress_b_a_b(){
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Total", (unsigned int) 4,p.total);
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Exclusion", true, q.isExcluded('a'));
 	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Exclusion", true, q.isExcluded('b'));
+}
+
+void ContextTableTest::testCompress_b_a_b_with_context() {
+	ContextTable c;
+	Query q;
+	
+	q.setTerm('a');
+	c.compress(q);
+	q.setTerm('b');
+	c.compress(q);
+	q.setTerm('b');
+	q.addExclusion('a');
+	c.compress(q);
+	
+	Probability p = q.getProbability();
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Found", true, q.isFound());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Skip",  (unsigned int) 0,p.skip);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Width", (unsigned int) 1,p.width);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Total", (unsigned int) 2,p.total);
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Exclusion", true, q.isExcluded('a'));
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("Bad Exclusion", true, q.isExcluded('b'));
+	
+	
 }
