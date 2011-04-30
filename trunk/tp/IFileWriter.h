5c995c0054cd4655af6cc953aac78bc53ff80404
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-04-30 19:14:09 +0000

    soporte de strings para writer y limpieza de objetos en tests

--- trunk/tp/IFileWriter.h
+++ trunk/tp/IFileWriter.h
@@ -1,6 +1,7 @@
 #ifndef _util_i_file_writer_h_
 #define _util_i_file_writer_h_
 
+#include <string>
 #include "util.h"
 
 namespace util {
@@ -8,6 +9,7 @@ namespace util {
 		public:
 			virtual ~IFileWriter(){};
 			virtual void write(char c)=0;
+			virtual void write(std::string s)=0;
 	};
 }
 #endif //_util_i_file_writer_h_
