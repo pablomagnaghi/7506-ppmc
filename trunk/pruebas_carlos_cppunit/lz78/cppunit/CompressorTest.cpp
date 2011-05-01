c2d89657a7fd656ab9cebf51f994e3c9813aed67
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-05-01 20:49:28 +0000

    lz78 comprime y descomprime ok, incluso con el caso particular de descompresion, pero no con n bits sino con una representacion ascii, apto para examen

--- trunk/pruebas_carlos_cppunit/lz78/cppunit/CompressorTest.cpp
+++ trunk/pruebas_carlos_cppunit/lz78/cppunit/CompressorTest.cpp
@@ -75,7 +75,33 @@ void CompressorTest::testCompress_a(){
 	delete c;
 }
 
-void CompressorTest::testCompress_ab(){
+void CompressorTest::testDecompress_a(){
+	string reference="a";
+	MockedFileReader* in = new MockedFileReader("a'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
+void CompressorTest::testDecompress_ab(){
+	string reference="ab";
+	MockedFileReader* in = new MockedFileReader("ab'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
+void CompressorTest::testCompress_aba(){
 	string reference="aba'256";
 	MockedFileReader* in = new MockedFileReader("aba");
 	MockedFileWriter* out = new MockedFileWriter();
@@ -87,6 +113,20 @@ void CompressorTest::testCompress_ab(){
 	delete out;
 	delete c;
 }
+
+void CompressorTest::testDecompress_aba(){
+	string reference="aba";
+	MockedFileReader* in = new MockedFileReader("aba'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
 void CompressorTest::testCompress_person(){
 	string reference="PEP/'257RSON'260'258E'262'266RT'256";
 	MockedFileReader* in = new MockedFileReader("PEP/PERSON/PEPERS/PERT");
@@ -99,6 +139,20 @@ void CompressorTest::testCompress_person(){
 	delete out;
 	delete c;
 }
+
+void CompressorTest::testDecompress_person(){
+	string reference="PEP/PERSON/PEPERS/PERT";
+	MockedFileReader* in = new MockedFileReader("PEP/'257RSON'260'258E'262'266RT'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
 void CompressorTest::testCompress_ABAABAAC() {
 	string reference="ABA'257'259C'256";
 	MockedFileReader* in = new MockedFileReader("ABAABAAC");
@@ -112,12 +166,38 @@ void CompressorTest::testCompress_ABAABAAC() {
 	delete c;
 }
 
+void CompressorTest::testDecompress_ABAABAAC() {
+	string reference="ABAABAAC";
+	MockedFileReader* in = new MockedFileReader("ABA'257'259C'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
 void CompressorTest::testCompress_ABAAABACABACA(){
 	string reference="ABA'259'258C'257A'262'256";
 	MockedFileReader* in = new MockedFileReader("ABAAABACABACA");
 	MockedFileWriter* out = new MockedFileWriter();
 	Compressor *c = new Compressor(10);
 	c->compress(in, out);
+	CPPUNIT_ASSERT_EQUAL(reference, out->get());
+
+	delete in;
+	delete out;
+	delete c;
+}
+
+void CompressorTest::testDecompress_ABAAABACABACA(){
+	string reference="ABAAABACABACA";
+	MockedFileReader* in = new MockedFileReader("ABA'259'258C'257A'262'256");
+	MockedFileWriter* out = new MockedFileWriter();
+	Compressor *c = new Compressor(10);
+	c->decompress(in, out);
 	CPPUNIT_ASSERT_EQUAL(reference, out->get());
 
 	delete in;
