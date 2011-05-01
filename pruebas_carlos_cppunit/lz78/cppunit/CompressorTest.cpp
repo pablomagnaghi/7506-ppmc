82933a8c7aec058a41089580b76e528b40eb3ffe
carlos.pantelides <carlos.pantelides@gmail.com>
2011-05-01 15:28:24 +0000

    lz78 comprime ok, pero no con n bits sino con una representacion ascii, apto para examen

--- pruebas_carlos_cppunit/lz78/cppunit/CompressorTest.cpp
+++ pruebas_carlos_cppunit/lz78/cppunit/CompressorTest.cpp
@@ -30,6 +30,38 @@ void CompressorTest::testConstructor(){
 	delete c;
   
 }
+
+void CompressorTest::testFind(){
+	Compressor *c = new Compressor(10);
+	string match;
+
+	match = '\0';
+	CPPUNIT_ASSERT_MESSAGE("Not found", c->find(match));
+
+	match = '\255';
+	CPPUNIT_ASSERT_MESSAGE("Not found", c->find(match));
+
+	CPPUNIT_ASSERT_MESSAGE("Not found", c->find("eof")); // toy value
+	CPPUNIT_ASSERT_MESSAGE("Bogus found", ! c->find("eof1")); // toy value
+	CPPUNIT_ASSERT_MESSAGE("Not found", c->find("/"));
+	CPPUNIT_ASSERT_MESSAGE("Bogus found", ! c->find("//"));
+
+	delete c;
+}
+
+void CompressorTest::testAdd(){
+	Compressor *c = new Compressor(10);
+	c->add("//");
+	CPPUNIT_ASSERT_MESSAGE("Not found",c->find("//"));
+	
+	CPPUNIT_ASSERT_MESSAGE("Bogus found", ! c->find("///"));
+	c->add("///");
+	CPPUNIT_ASSERT_MESSAGE("Not found",c->find("//"));
+	CPPUNIT_ASSERT_MESSAGE("Not found",c->find("///"));
+	
+	delete c;
+}
+
 void CompressorTest::testCompress_a(){
 	string reference="a'256";
 	MockedFileReader* in = new MockedFileReader("a");
@@ -56,7 +88,7 @@ void CompressorTest::testCompress_ab(){
 	delete c;
 }
 void CompressorTest::testCompress_ababab(){
-	string reference="PEP/'257RSON'260'258E262'266RT'256";
+	string reference="PEP/'257RSON'260'258E'262'266RT'256";
 	MockedFileReader* in = new MockedFileReader("PEP/PERSON/PEPERS/PERT");
 	MockedFileWriter* out = new MockedFileWriter();
 	Compressor *c = new Compressor(10);
