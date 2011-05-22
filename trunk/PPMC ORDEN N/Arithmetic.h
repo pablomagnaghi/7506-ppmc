27694d420a0fdcdcb7d5dd918bd6ed6bb5e86086
pany100@gmail.com <pany100@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-05-22 22:34:37 +0000

    Agregado de manejo de pisos, techos

--- trunk/PPMC ORDEN N/Arithmetic.h
+++ trunk/PPMC ORDEN N/Arithmetic.h
@@ -5,11 +5,15 @@
 #include "PPMC.h"
 #include "Model.h"
 #include "ContextSelector.h"
+#include "algorithm"
 
 namespace ppmc {
 	class Arithmetic {
 		public:
 			Arithmetic();
+			u_int64_t getTop();
+			u_int64_t getBottom();
+			void setNewLimits(u_int64_t, u_int64_t);
 			virtual ~Arithmetic();
 		protected:
 			std::vector<Model*> models;
@@ -17,6 +21,28 @@ namespace ppmc {
 			FrequencyTable frequencyTable;
 			//std::map<char, std::size_t> table;
 			// necesito mapa de probabilidades, <frecuencia, total>
+		private:
+			u_int64_t top;
+			u_int64_t bottom;
+			u_int64_t buffer;
+			u_int8_t bits_in_buffer;
+			u_int8_t underflow_counter;
+			/*
+			 * Agrego el bit al buffer
+			 */
+			void addBitToBuffer(u_int8_t);
+
+			/*
+			 * Funcion que se usa para mandar el buffer de bits al fileWriter
+			 */
+			void putBufferInFileWriter();
+			void solve_underflow();
+			void solve_overflow();
+
+			/*
+			 * Funcion que se llama al final del aritmetico para flushear los ultimos bits
+			 */
+			void clean_buffer();
 	};
 }
 #endif //_ppmc_arithmetic_h_
---
+++ trunk/PPMC ORDEN N/Constants.h
@@ -0,0 +1,17 @@
+/*
+ * Constants.h
+ *
+ *  Created on: 22/05/2011
+ *      Author: luis
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define TOP 0xFFFFFFFFFFFFFFFF
+#define BOTTOM 0x0000000000000000
+#define EMPTY_BUFFER 0x0000000000000000
+#define BYTE_128 0x8000000000000000
+#define BYTE_127 0x7FFFFFFFFFFFFFFF
+
+#endif /* CONSTANTS_H_ */
