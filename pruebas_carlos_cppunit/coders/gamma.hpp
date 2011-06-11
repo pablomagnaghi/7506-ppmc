4f8aac762fec11155ad7f26067b38335f69ee4fc
carlos.pantelides <carlos.pantelides@gmail.com>
2011-06-11 16:04:32 +0000

    tag previo a merge ut arithmetic

--- pruebas_carlos_cppunit/coders/gamma.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __gamma_h__
-#define __gamma_h__
-
-void gamma_encode(int number, int* gamma_value, int* gamma_len );
-
-void gamma_decode(int number);
-
-#endif /* __gamma_h__ */
--- pruebas_carlos_cppunit/hex_coder/v04/test/cleanup.sh
+++
@@ -1,3 +0,0 @@
-cd test
-rm out.*
-cd ..
