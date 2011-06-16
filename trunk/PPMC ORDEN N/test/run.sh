157275ad5e68b2d92d3d32f2d2707d694149edb8
carlos.pantelides@gmail.com <carlos.pantelides@gmail.com@591442b1-963f-6784-1635-8defa45f4929>
2011-06-16 01:27:48 +0000

    test para valgrind

--- trunk/PPMC ORDEN N/test/run.sh
+++ trunk/PPMC ORDEN N/test/run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
+valgrind ./ppmc -c test/fixture/short_binary -s test/tmp/short_binary.z 2>&1 \
+| grep -q -e "All heap blocks were freed -- no leaks are possible" && echo -n "." || echo "Failed VALGRIND" && exit 1
+
 for FILE in test/fixture/*; do
   TARGET=$(basename $FILE)
   ./ppmc -c $FILE -s test/tmp/$TARGET.z
   ./ppmc -x test/tmp/$TARGET.z -s test/tmp/$TARGET.original
-   cmp $FILE test/tmp/$TARGET.original && echo -n "." || ( echo "Failed $FILE" && exit 1)
+   cmp $FILE test/tmp/$TARGET.original && echo -n "." ||  echo "Failed $FILE" && exit 1
 done
 echo
